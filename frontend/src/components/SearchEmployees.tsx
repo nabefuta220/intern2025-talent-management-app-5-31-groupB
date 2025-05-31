54e95f7bd1681e474d765de0ff7bbdc5221c8682
nabefuta <satoshinabe220@gmail.com>
2025-05-31 16:55:52 +0900

    追加ボタンを追加

--- frontend/src/components/SearchEmployees.tsx
+++ frontend/src/components/SearchEmployees.tsx
@@ -1,10 +1,39 @@
 "use client";
 import { Paper, TextField } from "@mui/material";
+import { Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { green } from '@mui/material/colors';
 import { useState } from "react";
+import { SxProps } from '@mui/system';
 import { EmployeeListContainer } from "./EmployeeListContainer";
 
 export function SearchEmployees() {
   const [searchKeyword, setSearchKeyword] = useState("");
+  const fabStyle = {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  };
+  const fabGreenStyle = {
+    color: 'common.white',
+    bgcolor: green[500],
+    '&:hover': {
+      bgcolor: green[600],
+    },
+  };
+  const fab = 
+    {
+      color: 'inherit' as 'inherit',
+      sx:{ ...fabStyle, ...fabGreenStyle } as SxProps,
+      icon: <AddIcon />,
+      label: 'Add',
+    }
+  
+  const handleAddEmployees = () =>
+    {
+      console.log("Add Employees clicked");
+    };
+
   return (
     <Paper
       sx={{
@@ -24,6 +53,10 @@ export function SearchEmployees() {
         key="employeesContainer"
         filterText={searchKeyword}
       />
+      <Fab sx={fab.sx} aria-label={fab.label} color={fab.color} onClick={ handleAddEmployees }>
+        {fab.icon}
+          </Fab>
+  
     </Paper>
   );
 }
